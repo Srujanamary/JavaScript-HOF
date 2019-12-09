56ce79a6d1f4d21061c574281f3277cd9e84be41
M_688776 <jason.monroe@cognizant.com>
2019-12-09 13:03:05 -0600

    first

---
+++ hof.js
@@ -0,0 +1,12 @@
+import volcanoes from './volcanoes.js';
+
+class VolcanoAnalyzer{
+
+    eruptedIn70s(){
+        
+    }
+}
+
+const volcanoAnalyzer = new VolcanoAnalyzer();
+console.log("---Erupted in 70's---");
+console.log(volcanoAnalyzer.eruptedIn70s());
